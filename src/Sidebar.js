9e56cdb251dfd16a7b3438aa4e4e62f81fab3ba2
diphokoo <diphokoo@outlook.com>
2026-02-24 08:08:24 +0200

    Refactor Sidebar component to include dynamic menu items and improve layout

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,39 @@
 import React from "react";
 
-function Sidebar() {
+const Sidebar = ({ activeItem, setActiveItem }) => {
+  const menuItems = [
+    "Dashboards",
+    "Product",
+    "Store",
+    "Messages",
+    "Statics",
+    "Invoices",
+    "To Do List",
+    "Finances",
+  ];
+
   return (
-    <div className="sidebar">
-      <h2>Sidebar</h2>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+    <div className="d-flex flex-column vh-100 p-3">
+      <h4 className="text-center mb-4">MyApp</h4>
+
+      <ul className="nav nav-pills flex-column mb-auto">
+        {menuItems.map((item) => (
+          <li className="nav-item" key={item}>
+            <button
+              className={`nav-link text-start w-100 ${
+                activeItem === item
+                  ? "active bg-primary text-white"
+                  : "text-white"
+              }`}
+              onClick={() => setActiveItem(item)}
+            >
+              {item}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
-}   
+};
 
 export default Sidebar;
